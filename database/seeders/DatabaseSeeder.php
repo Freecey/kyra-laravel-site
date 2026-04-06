0d0982dbc4854292ec9b7235a7d7d401b45bd85f
Cedric AUDRIT <cedric@audrit.be>
2026-04-06 23:31:15 +0200

    chore: ajout ManifesteSeeder au DatabaseSeeder (idempotent via firstOrCreate)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             AdminUserSeeder::class,
             SettingSeeder::class,
             FirstPostSeeder::class,
+            ManifesteSeeder::class,
         ]);
     }
 }
